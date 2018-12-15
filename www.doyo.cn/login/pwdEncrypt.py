d5bd7e5e836cb969421cb24efc97732f4a76ce42
fraaannn <wellplay666@outlook.com>
2018-12-15 23:51:21 +0800

    Update pwdEncrypt.py
    update

---
+++ www.doyo.cn/login/pwdEncrypt.py
@@ -0,0 +1,33 @@
+import random
+import json
+
+import requests
+import execjs
+
+def get_js():
+    with open('./pwdEncrypt.js', 'r', encoding='utf-8') as f:
+        js_str = f.read()
+    return js_str
+
+def get_data(account):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+        'Referer': 'http://www.doyo.cn/passport/login',
+    }
+    url = 'http://www.doyo.cn/User/Passport/token?username={0}&random={1}'.format(account, str(random.random())[0:17])
+    r = requests.get(url, headers=headers)
+    data = json.loads(r.text)
+    return data
+
+def get_encrypt_pwd(account, password):
+    data = get_data(account)
+    js_str = get_js()
+    ctx = execjs.compile(js_str)
+    encrypt_pwd = ctx.call(password, data['nonce'], data['ts'])
+    return encrypt_pwd
+
+if __name__ == '__main__':
+    account = ''
+    pwd = ''
+    encrypt_pwd = get_encrypt_pwd(account, pwd)
+    print(encrypt_pwd)
